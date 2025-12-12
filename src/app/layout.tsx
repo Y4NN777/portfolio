eadab24307c47ef288b2192592feef68bf040b28
Y4NN777 <axel.studiesmail@gmail.com>
2025-12-12 23:58:22 +0000

    remove temp folder and improve SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Space_Grotesk({ subsets: ["latin"], weight: ["500", "600", "700"] 
 
 export const metadata: Metadata = {
   title: "TheY4NN, the777th Dev",
-  description: "Portfolio of the Y4NN, a passionate backend/full-stack developer specializing in modern web technologies, React, Next.js, and innovative digital solutions.",
+  description: "Portfolio of the Y4NN, a curious backend/full-stack developer specializing in modern software engineering, AI engineering.",
   keywords: "Ragnang-Newende Yanis Axel DABO, Backend Developer, GenIA, AgenticAI, Web3, Software Engineer, Systems Engineer, Portfolio",
   authors: [{ name: "Ragnang-Newende Yanis Axel DABO" }],
   icons: {
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
   },
   openGraph: {
     title: "Ragnang-Newende Yanis Axel DABO - Backend Developer, GenIA & AgenticAI Enthusiast, Web3 Enthusiast, Aspiring Software and Systems Engineer",
-    description: "Portfolio of the Y4NN, a passionate developer specializing in modern and smart systems building.",
+    description: "Portfolio site of the Y4NN, a curious developer specializing in secured and performant smart systems building.",
     type: "website",
   },
 };
